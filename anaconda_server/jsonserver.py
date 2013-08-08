b52f1b28bdc72a3d1867b437384d8c33fbfbdfd3
Oscar Campos <oscar.campos@dedsert.com>
2013-08-08 17:49:36 +0100

    removed unused import in jsonserver, redefined Windows key mapping

--- anaconda_server/jsonserver.py
+++ anaconda_server/jsonserver.py
@@ -29,7 +29,6 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 
 import jedi
 from linting import linter
-from decorators import timeit
 from contexts import json_decode
 
 DEBUG_MODE = False
