18f600ff96c472a4c33fbef4622a7d503558d083
Oscar Campos <oscar.campos@member.fsf.org>
2013-08-09 18:07:58 +0100

    removed view object references from Worker class

--- utils.py
+++ utils.py
@@ -16,3 +16,23 @@ def get_settings(view, name, default=None):
 
     plugin_settings = sublime.load_settings('Anaconda.sublime-settings')
     return view.settings().get(name, plugin_settings.get(name, default))
+
+
+def active_view():
+    """Return the active view
+    """
+
+    return sublime.active_window().active_view()
+
+
+def prepare_send_data(location):
+    """Prepare dict that has to be sended trough the socket
+    """
+
+    view = active_view()
+    return {
+        'source': view.substr(sublime.Region(0, view.size())),
+        'line': location[0] + 1,
+        'offset': location[1],
+        'filename': view.file_name() or ''
+    }
