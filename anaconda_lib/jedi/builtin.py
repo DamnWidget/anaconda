796588374f5d330330f76b15d24acb2403bfe80a
@damnwidget <oscar.campos@member.fsf.org>
2013-12-23 02:29:44 +0000

    totally fixed problem with PyQt on Jedi library itself. this is related with issue #108

--- anaconda_lib/jedi/builtin.py
+++ anaconda_lib/jedi/builtin.py
@@ -364,7 +364,7 @@ def _parse_function_doc(func):
                 end = start + i
                 break
         param_str = doc[start + 1:end]
-    except (ValueError, UnboundLocalError):
+    except (ValueError, UnboundLocalError, AttributeError):
         # ValueError for doc.index
         # UnboundLocalError for undefined end in last line
         debug.dbg('no brackets found - no param')
