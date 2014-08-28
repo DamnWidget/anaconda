997fd8bc91172a7cbd6e77466db5935cb5718946
Oscar Campos <oscar.campos@member.fsf.org>
2014-08-28 21:40:31 +0100

    removed unused imports from handlers

--- anaconda_server/handlers/autoformat_handler.py
+++ anaconda_server/handlers/autoformat_handler.py
@@ -3,9 +3,6 @@
 # Copyright (C) 2014 - Oscar Campos <oscar.campos@member.fsf.org>
 # This program is Free Software see LICENSE file for details
 
-import os
-import sys
-
 from commands import AutoPep8
 from lib.anaconda_handler import AnacondaHandler
 
