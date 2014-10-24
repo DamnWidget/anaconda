0d154d7fc1db6624417fd1d3bf31eb94a8002dd8
Oscar Campos <oscar.campos@member.fsf.org>
2014-10-24 16:59:43 +0100

    first version

---
+++ javascript/anaconda.js
@@ -0,0 +1,82 @@
+/*!
+ * Adapted from BootStrap documentation JavaScript
+ *
+ * JavaScript for Bootstrap's docs (http://getbootstrap.com)
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+ * details, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+/*!
+ * Adapted from Bootstrap docs JavaScript
+ */
+
+
+!function ($) {
+
+  $(function () {
+
+    // IE10 viewport hack for Surface/desktop Windows 8 bug
+    //
+    // See Getting Started docs for more information
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+      var msViewportStyle = document.createElement('style')
+      msViewportStyle.appendChild(
+        document.createTextNode(
+          '@-ms-viewport{width:auto!important}'
+        )
+      )
+      document.querySelector('head').appendChild(msViewportStyle)
+    }
+
+    var $window = $(window)
+    var $body   = $(document.body)
+
+    $body.scrollspy({
+      target: '.sidebar'
+    });
+
+    $window.on('load', function () {
+      $body.scrollspy('refresh')
+    });
+
+    $('.sidebar').bind('contextmenu', function(e) {
+      e.preventDefault();
+    });
+
+    $('.anaconda-container [href]').click(function (e) {
+      // hugo/blackfriday sucks hard
+      if (~this.href.indexOf("#toc_")) {
+        e.preventDefault();
+        var i = this.href.indexOf("#toc_");
+        document.location.href = document.location.pathname + this.href.substr(i);
+      }
+    });
+
+    // back to top
+    setTimeout(function () {
+      var $sideBar = $('.sidebar')
+
+      $sideBar.affix({
+        offset: {
+          top: function () {
+            var offsetTop      = $sideBar.offset().top
+            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+            var navOuterHeight = $('.docs-nav').height()
+
+            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+          },
+          bottom: function () {
+            return (this.bottom = $('.footer').outerHeight(true))
+          }
+        }
+      })
+    }, 100);
+
+    setTimeout(function () {
+      $('.top').affix()
+    }, 100);
+
+  })
+
+}(jQuery)
