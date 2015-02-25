151f948ee6bfc6747637074673eea025a130c085
Oscar Campos <oscar.campos@member.fsf.org>
2015-02-25 16:40:56 +0000

    added support for new tooltips

--- anaconda_server/commands/doc.py
+++ anaconda_server/commands/doc.py
@@ -11,14 +11,16 @@ class Doc(Command):
     """Get back a python definition where to go
     """
 
-    def __init__(self, callback, uid, script):
+    def __init__(self, callback, uid, script, html):
         self.script = script
+        self.html = html
         super(Doc, self).__init__(callback, uid)
 
     def run(self):
         """Run the command
         """
 
+        processed = []
         try:
             definitions = self.script.goto_definitions()
         except Exception as error:
@@ -29,16 +31,33 @@ class Doc(Command):
             success = False
             docs = []
         else:
+            docs = []
             success = True
-            docs = [
-                'Docstring for {0}\n{1}\n{2}'.format(
-                    d.full_name, '=' * 40, d.doc
-                ) if d.doc else 'No docstring for {0}'.format(d)
-                for d in definitions
-            ]
+            for definition in definitions:
+                if definition not in processed:
+                    docs.append(
+                        self._plain(definition) if not self.html
+                            else self._html(definition)
+                    )
+                    processed.append(definition)
 
         self.callback({
             'success': success,
-            'doc': ('\n' + '-' * 79 + '\n').join(docs),
+            'doc': ('<br><br>' if self.html else '\n'+'-'*79+'\n').join(docs),
             'uid': self.uid
         })
+
+    def _plain(sef, definition):
+        """Generate a documentation string for use as plain text
+        """
+
+        return 'Docstring for {0}\n{1}\n{2}'.format(
+            definition.full_name, '=' * 40, definition.doc
+        )
+
+    def _html(self, definition):
+        """Generate documentation string in HTML format
+        """
+
+        return '{0}\n{1}'.format(
+            definition.full_name, definition.doc.replace('\n', '<br>'))
