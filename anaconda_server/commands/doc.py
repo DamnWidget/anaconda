DamnWidget/anaconda

commit 2ba029f4a48a2fa98569eed5c4a932bb6039c22b

Fix tooltips for function signatures and docstrings which contain special HTML characters. Fixes #417

anaconda_server/commands/doc.py
@@ -3,6 +3,7 @@
 # This program is Free Software see LICENSE file for details
 
 import logging
+import html
 
 from .base import Command
 
@@ -58,6 +59,7 @@ class Doc(Command):
     def _html(self, definition):
         """Generate documentation string in HTML format
         """
+        escaped_doc = html.escape(html.unescape(definition.doc), quote=False)
+        escaped_doc = escaped_doc.replace('\n', '<br>')
 
-        return '{0}\n{1}'.format(
-            definition.full_name, definition.doc.replace('\n', '<br>'))
+        return '{0}\n{1}'.format(definition.full_name, escaped_doc)

listeners/signatures.py
@@ -60,12 +60,14 @@ class AnacondaSignaturesEventListener(sublime_plugin.EventListener):
                 i = data['doc'].split('<br>').index("")
             except ValueError:
                 self.signature = data['doc']
+                self.doc = ''
                 if show_tooltip and show_doc and st_version >= 3070:
                     return self._show_popup(view)
                 return self._show_status(view)
 
             if show_tooltip and show_doc and st_version >= 3070:
                 self.doc = '<br>'.join(data['doc'].split('<br>')[i:])
+                self.doc = self.doc.replace("  ", "&nbsp;&nbsp;")
 
             if not show_tooltip or st_version < 3070:
                 self.signature = data['doc'].splitlines()[2]

commit 58106683588cd39ec42f808db7f1ac2eb355b6fd

fix broken python2 support introduced by PR #469, fixes #472

anaconda_server/commands/doc.py
@@ -2,8 +2,15 @@
 # Copyright (C) 2013 - Oscar Campos <oscar.campos@member.fsf.org>
 # This program is Free Software see LICENSE file for details
 
+import sys
 import logging
-import html
+
+try:
+    import html
+except ImportError:
+    # python2 faillback
+    import cgi
+    from HTMLParser import HTMLParser
 
 from .base import Command
 
@@ -16,6 +23,7 @@ class Doc(Command):
         self.script = script
         self.html = html
         super(Doc, self).__init__(callback, uid)
+
 
     def run(self):
         """Run the command
@@ -59,7 +67,14 @@ class Doc(Command):
     def _html(self, definition):
         """Generate documentation string in HTML format
         """
-        escaped_doc = html.escape(html.unescape(definition.doc), quote=False)
+
+        if sys.version_info >= (3, 0):
+            escaped_doc = html.escape(
+                html.unescape(definition.doc), quote=False)
+        else:
+            escaped_doc = cgi.escape(
+                HTMLParser.unescape.__func__(HTMLParser, definition.doc))
+
         escaped_doc = escaped_doc.replace('\n', '<br>')
 
         return '{0}\n{1}'.format(definition.full_name, escaped_doc)