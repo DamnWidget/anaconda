94e654acad6d6f74dc471ee7adfa4cf8c9a538a7
Oscar Campos <oscar.campos@member.fsf.org>
2016-05-02 18:29:35 +0100

    do not execute setup actions while in ST3 context

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup, find_packages
+try:
+    # don't execute this in sublime text
+    import sublime
+except ImportError:
+    from setuptools import setup, find_packages
 
-setup(
-    name='anaconda_tests',
-    packages=find_packages(),
-    namespace_packages=['test']
-)
+    setup(
+        name='anaconda_tests',
+        packages=find_packages(),
+        namespace_packages=['test']
+    )
