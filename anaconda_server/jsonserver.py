9e6c2986ee783278279fc583ca4f155b09a8e35b
Oscar Campos <oscar.campos@member.fsf.org>
2016-10-04 19:34:33 +0100

    fixed wrong logic to create non existing dirs in jsonsever

--- anaconda_server/jsonserver.py
+++ anaconda_server/jsonserver.py
@@ -337,7 +337,7 @@ if __name__ == "__main__":
             server = JSONServer(('localhost', port))
         else:
             unix_socket_path = UnixSocketPath(options.project)
-            if not os.path.exists(unix_socket_path.socket):
+            if not os.path.exists(os.path.dirname(unix_socket_path.socket)):
                 os.makedirs(os.path.dirname(unix_socket_path.socket))
             if os.path.exists(unix_socket_path.socket):
                 os.unlink(unix_socket_path.socket)
