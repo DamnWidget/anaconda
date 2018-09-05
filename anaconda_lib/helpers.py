e6b7cc6e8f203d9dede651270b34bb5618d8d9ee
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2018-09-05 13:14:56 +0000

    Add setting to allow multiple .anaconda files

--- anaconda_lib/helpers.py
+++ anaconda_lib/helpers.py
@@ -208,11 +208,19 @@ def get_settings(view, name, default=None):
     if (name in ('python_interpreter', 'extra_paths') and not
             ENVIRON_HOOK_INVALID[view.id()]):
         if view.window() is not None and view.window().folders():
-            dirname = os.path.dirname(view.file_name())
+            allow_multiple_env_hooks = get_settings(
+                view,
+                'anaconda_allow_project_environment_hooks',
+                False
+            )
+            if allow_multiple_env_hooks:
+                dirname = os.path.dirname(view.file_name())
+            else:
+                dirname = view.window().folders()[0]
             while True:
                 environfile = os.path.join(dirname, '.anaconda')
                 if os.path.exists(environfile) and os.path.isfile(environfile):
-                    # print("Environ found on %s" % environfile)
+                    print("Environ found on %s" % environfile)
                     with open(environfile, 'r') as jsonfile:
                         try:
                             data = json.loads(jsonfile.read())
